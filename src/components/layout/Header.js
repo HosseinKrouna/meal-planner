2316d77040b52082e2a98ebfbb79a36346f9712c
Nicole Bendig <nicoleb@inhouse.nterra.com>
2023-12-27 20:13:04 +0100

    modify: restructured the header

---
+++ src/components/layout/Header.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+function Header() {
+	return (
+		<header className="flex items-center justify-between">
+			<Link href={""} className="text-primary font-semibold text-2xl">
+				MEAL-PLANNER
+			</Link>
+			<nav className="flex items-center gap-6 text-gray-500 font-semibold">
+				<Link href={""}>Home</Link>
+				<Link href={""}>Menu</Link>
+				<Link href={""}>About</Link>
+				<Link href={""}>Contact</Link>
+				<Link
+					href={""}
+					className="bg-primary text-white rounded-full px-8 py-2"
+				>
+					Login
+				</Link>
+			</nav>
+		</header>
+	);
+}
+
+export default Header;
