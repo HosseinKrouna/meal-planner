0514ae5a705ee89f8987ede403737e6a37126539
Nicole Bendig <nicoleb@inhouse.nterra.com>
2023-12-28 20:07:15 +0100

    add: Header and footer created for the registration and login page

---
+++ src/app/register/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+function ReigisterPage() {
+	return (
+		<section className="text-center text-primary text-4xl">
+			<h1>Register</h1>
+		</section>
+	);
+}
+
+export default ReigisterPage;
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import { useState } from "react";
 import Menu from "../icons/Menu";
@@ -12,31 +14,16 @@ function Header() {
 	return (
 		<header className="flex items-center justify-between p-4 md:p-8">
 			<div className="flex items-center">
-				<Link href={""} className="text-primary font-semibold text-2xl">
+				<Link href={"/"} className="text-primary font-semibold text-2xl">
 					MEAL-PLANNER
 				</Link>
-			</div>
-
-			<div className="flex items-center ml-4">
-				<Link
-					href={""}
-					className="bg-primary text-white rounded-full px-4 py-2 md:hidden"
-				>
-					Login
-				</Link>
-			</div>
-
-			<div className="items-center md:hidden">
-				<button onClick={toggleMenu} className="focus:outline-none">
-					<Menu />
-				</button>
 			</div>
 
 			{/*Mobile Navigation*/}
 			{showMenu && (
 				<div className="fixed top-16 right-4 z-50 bg-white rounded shadow-xl p-4">
 					<Link
-						href={""}
+						href={"/"}
 						className="block py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
 					>
 						Home
@@ -65,16 +52,24 @@ function Header() {
 			{/* Desktop Navigation */}
 			<nav className="hidden md:flex items-center gap-6 text-gray-500 font-semibold">
 				<Link href={""}>Home</Link>
-				<Link href={""}>Menu</Link>
+				<Link href={""}>Menü</Link>
 				<Link href={""}>About</Link>
 				<Link href={""}>Contact</Link>
+			</nav>
+			<nav className="flex items-center gap-4 text-gray-500 font-semibold">
+				<Link href={"/login"}>Login</Link>
 				<Link
-					href={""}
+					href={"/register"}
 					className="bg-primary text-white rounded-full px-4 py-2"
 				>
-					Login
+					Register
 				</Link>
 			</nav>
+			<div className="items-center md:hidden">
+				<button onClick={toggleMenu} className="focus:outline-none">
+					<Menu />
+				</button>
+			</div>
 		</header>
 	);
 }
