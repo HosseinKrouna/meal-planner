53db49179c4a5eb39ce6771f35d859b0ea23e25a
Nicole Bendig <nicoleb@inhouse.nterra.com>
2023-12-28 09:25:46 +0100

    modify: Screen responsive adapted to mobile version

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 function Header() {
 	return (
-		<header className="flex items-center justify-between">
+		<header className="flex items-center justify-between p-4 md:p-8">
 			<Link href={""} className="text-primary font-semibold text-2xl">
 				MEAL-PLANNER
 			</Link>
-			<nav className="flex items-center gap-6 text-gray-500 font-semibold">
+			<nav className="hidden md:flex items-center gap-6 text-gray-500 font-semibold">
 				<Link href={""}>Home</Link>
 				<Link href={""}>Menu</Link>
 				<Link href={""}>About</Link>
 				<Link href={""}>Contact</Link>
 				<Link
 					href={""}
-					className="bg-primary text-white rounded-full px-8 py-2"
+					className="bg-primary text-white rounded-full px-4 py-2"
 				>
 					Login
 				</Link>
--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -2,31 +2,31 @@ import Image from "next/image";
 
 function Hero() {
 	return (
-		<section className="grid grid-cols-2">
-			<div>
-				<h1 className="text-4xl font-semibold">
+		<section className="grid grid-cols-1 md:grid-cols-2 p-4 md:p-8 max-w-full md:max-w-screen-lg mx-auto overflow-x-hidden">
+			<div className="order-2 md:order-1">
+				<h1 className="text-2xl md:text-4xl font-semibold mb-4 md:mb-6">
 					Gesunde Speisepläne für ein besseres Leben
 				</h1>
-				<p className="mt-4 text-gray-500">
+				<p className="text-sm md:text-base text-gray-500 mb-4 md:mb-6">
 					Entdecke in unserer App VitalitätsKüche eine Fülle an köstlichen,
 					ausgewogenen Speiseplänen, die deine Gesundheit fördern und dir
 					helfen, deine Ziele zu erreichen – egal, ob du abnehmen, mehr Energie
 					haben oder einfach gesünder leben möchtest!
 				</p>
-				<div className="flex gap-4">
+				<div className="flex flex-col md:flex-row gap-4">
 					<button className="bg-primary text-white px-8 py-2 rounded-full">
 						Wähle jetzt
 					</button>
-					<button>Learn more</button>
+					<button className="border px-4 py-2 rounded-full">Learn more</button>
 				</div>
 			</div>
 
-			<div className="relative">
+			<div className="relative order-1 md:order-2">
 				<Image
 					src="/photo-hero-food.png"
 					alt="picture of different vegetables"
-					layout="fill"
-					objectFit="contain"
+					width={500}
+					height={300}
 				/>
 			</div>
 		</section>
