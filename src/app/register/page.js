3f9f925daad13f3805c88e66be54ab9bbcea98af
Nicole Bendig <nicoleb@inhouse.nterra.com>
2023-12-29 12:00:47 +0100

    install: next-auth; add: Provider for register

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,13 +1,29 @@
 "use client";
 import Image from "next/image";
+import { useState } from "react";
 
 function ReigisterPage() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	function handleFormSubmit() {}
+
 	return (
 		<section className="mt-10">
 			<h1 className="text-center mb-4 text-primary text-4xl">Register</h1>
-			<form className="block max-w-xs mx-auto">
-				<input type="email" placeholder="email" />
-				<input type="password" placeholder="password" />
+			<form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+				<input
+					type="email"
+					placeholder="email"
+					value={email}
+					onChange={(ev) => setEmail(ev.target.value)}
+				/>
+				<input
+					type="password"
+					placeholder="password"
+					value={password}
+					onChange={(ev) => setPassword(ev.target.value)}
+				/>
 				<button type="submit">Register</button>
 				<div className="my-4 text-center text-gray-600">
 					oder Log dich mit einem Provider an
