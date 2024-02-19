f534c05e0ac989306b22ff7d3def6d8424515ba0
Nicole Bendig <nicoleb@inhouse.nterra.com>
2024-02-19 19:33:22 +0100

    fix-vercel: Outsource authOptions

---
+++ src/libs/authOptions.js
@@ -0,0 +1,29 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/libs/mongoConnect";
+import { authenticate } from "@/libs/authenticate";
+
+export const authOptions = {
+	secret: process.env.SECRET,
+	adapter: MongoDBAdapter(clientPromise),
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+		}),
+		CredentialsProvider({
+			name: "Credentials",
+			id: "credentials",
+			credentials: {
+				username: {
+					label: "Email",
+					type: "email",
+					placeholder: "test@example.com",
+				},
+				password: { label: "Password", type: "password" },
+			},
+			authorize: authenticate,
+		}),
+	],
+};
