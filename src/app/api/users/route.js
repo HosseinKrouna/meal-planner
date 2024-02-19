70b89d6f188e1cfe901badbb4f9c7f35336381fa
Nicole Bendig <nicoleb@inhouse.nterra.com>
2024-02-19 19:46:13 +0100

    excluded isAdmin GET

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,14 +1,14 @@
-import { isAdmin } from "@/app/api/auth/[...nextauth]/route";
+// import { isAdmin } from "@/app/api/auth/[...nextauth]/route";
 import { User } from "@/models/User";
 import mongoose from "mongoose";
 
 export async function GET(req) {
 	mongoose.connect(process.env.MONGO_URL);
 
-	if (await isAdmin(req)) {
-		const users = await User.find();
-		return Response.json(users);
-	} else {
-		return Response.json([]);
-	}
+	// if (await isAdmin(req)) {
+	const users = await User.find();
+	return Response.json(users);
+	// } else {
+	// 	return Response.json([]);
+	// }
 }
