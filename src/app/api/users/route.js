127f966b2f145200c2d30375b11d93473576f17d
Nicole Bendig <nicoleb@inhouse.nterra.com>
2024-02-20 16:12:27 +0100

    optimize mongodb and users-route

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -3,12 +3,24 @@ import { User } from "@/models/User";
 import mongoose from "mongoose";
 
 export async function GET(req) {
-	mongoose.set("debug", true);
 	mongoose.connect(process.env.MONGO_URL);
 
-	const users = await User.find();
-	return Response.json(users);
+	try {
+		const users = await User.find().lean();
+		return Response.json(users);
+	} catch (error) {
+		console.error("Error fetching users:", error);
+		return Response.json([]);
+	}
 }
+
+// export async function GET(req) {
+// 	mongoose.set("debug", true);
+// 	mongoose.connect(process.env.MONGO_URL);
+
+// 	const users = await User.find();
+// 	return Response.json(users);
+// }
 
 // export async function GET(req) {
 // 	mongoose.connect(process.env.MONGO_URL);
