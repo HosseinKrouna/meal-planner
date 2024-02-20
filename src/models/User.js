972f6a85dcda1e398178232d53e68efa2084b345
Nicole Bendig <nicoleb@inhouse.nterra.com>
2024-02-20 16:12:27 +0100

    optimize mongodb and users-route

--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,7 @@ import { model, models, Schema } from "mongoose";
 
 const UserSchema = new Schema(
 	{
-		email: { type: String, required: true, unique: true },
+		email: { type: String, required: true, unique: true, index: true },
 		name: { type: String },
 		password: { type: String },
 		image: { type: String },
