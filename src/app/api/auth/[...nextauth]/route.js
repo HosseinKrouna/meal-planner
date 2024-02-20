92f73adfe8648b0591d85fce6284049fdf0ecf36
Nicole Bendig <nicoleb@inhouse.nterra.com>
2024-02-20 06:06:59 +0100

    excluded export isAdmin

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -5,7 +5,7 @@ import bcrypt from "bcrypt";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@/libs/mongoConnect";
-import { UserInfo } from "@/models/UserInfo";
+// import { UserInfo } from "@/models/UserInfo";
 
 async function authenticate(credentials) {
 	const email = credentials?.email;
@@ -47,18 +47,18 @@ export const authOptions = {
 	],
 };
 
-export async function isAdmin() {
-	const session = await getServerSession(authOptions);
-	const userEmail = session?.user?.email;
-	if (!userEmail) {
-		return false;
-	}
-	const userInfo = await UserInfo.findOne({ email: userEmail });
-	if (!userInfo) {
-		return false;
-	}
-	return userInfo.admin;
-}
+// export async function isAdmin() {
+// 	const session = await getServerSession(authOptions);
+// 	const userEmail = session?.user?.email;
+// 	if (!userEmail) {
+// 		return false;
+// 	}
+// 	const userInfo = await UserInfo.findOne({ email: userEmail });
+// 	if (!userInfo) {
+// 		return false;
+// 	}
+// 	return userInfo.admin;
+// }
 
 const handler = NextAuth(authOptions);
 
