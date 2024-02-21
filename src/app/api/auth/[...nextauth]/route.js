fa7e18b76901f94e8db2f9b38aa110485c7b1d21
Nicole Bendig <nicoleb@inhouse.nterra.com>
2024-02-21 16:53:47 +0100

    fix: conflicts resolved

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import NextAuth, { getServerSession } from "next-auth";
 import { UserInfo } from "@/models/UserInfo";
 import { authOptions } from "@/libs/authOptions";
@@ -15,24 +14,6 @@ export async function isAdmin() {
 	}
 	return userInfo.admin;
 }
-=======
-import NextAuth from "next-auth";
-import { authOptions } from "@/libs/authConfig";
-// import { UserInfo } from "@/models/UserInfo";
-
-// export async function isAdmin() {
-// 	const session = await getServerSession(authOptions);
-// 	const userEmail = session?.user?.email;
-// 	if (!userEmail) {
-// 		return false;
-// 	}
-// 	const userInfo = await UserInfo.findOne({ email: userEmail });
-// 	if (!userInfo) {
-// 		return false;
-// 	}
-// 	return userInfo.admin;
-// }
->>>>>>> dfcf75c55a278ce8a9621aa64e3d66b6250ca642
 
 const handler = NextAuth(authOptions);
 
